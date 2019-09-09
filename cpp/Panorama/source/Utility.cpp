cf90bcabb9bdb80cf90f31aced2cd84b9ccf61b5
Pratool Gadtaula <5598110+Pratool@users.noreply.github.com>
2019-09-09 00:34:12 -0400

    feat(panorama): Write uncropped blended image

--- cpp/Panorama/source/Utility.cpp
+++ cpp/Panorama/source/Utility.cpp
@@ -12,6 +12,60 @@
 
 #include "Panorama/Utility.hpp"
 
+struct WarpBounds
+{
+    cv::Size size{0, 0};
+    double minWidth;
+    double minHeight;
+};
+
+WarpBounds getWarpedBounds(const cv::Size &sourceImageSize,
+                           const cv::Mat &homography)
+{
+    // Set the output of sourceWarped to be the max height and max width of the
+    // warped image.  Due to the homography property that straight lines will
+    // stay straight after being transformed, this can be determined by getting
+    // the coordinates of the four corners of the image after being transformed.
+    // The maximum x and y-coordinates of the four remapped points will be used.
+
+    // Each of these points will be represented in homogenous coordinates.
+    // Simply appending 1 assumes the imager plane is of unit distance from the
+    // viewer.
+    std::array<cv::Vec3d, 4> corners({
+        cv::Vec3d(0, 0, 1),
+        cv::Vec3d(sourceImageSize.width, 0, 1),
+        cv::Vec3d(0, sourceImageSize.height, 1),
+        cv::Vec3d(sourceImageSize.width, sourceImageSize.height, 1)});
+
+    double minWidth = std::numeric_limits<double>::infinity();
+    double minHeight = std::numeric_limits<double>::infinity();
+    double maxWidth = -std::numeric_limits<double>::infinity();
+    double maxHeight = -std::numeric_limits<double>::infinity();
+    for (auto &corner : corners)
+    {
+        // Transform corner. Note that values in the output matrix can be
+        // negative!
+        const auto tmpCorner = cv::Mat(homography * corner);
+
+        // Convert homogenous coordinates back to the imager plane coordinates.
+        const auto &z = tmpCorner.at<double>(0, 2);
+        cv::Point2d tmpPoint(tmpCorner.at<double>(0, 0)/z,
+                             tmpCorner.at<double>(0, 1)/z);
+
+        minWidth = std::min(tmpPoint.x, minWidth);
+        minHeight = std::min(tmpPoint.y, minHeight);
+
+        maxWidth = std::max(tmpPoint.x, maxWidth);
+        maxHeight = std::max(tmpPoint.y, maxHeight);
+    }
+
+    return WarpBounds{
+        cv::Size{static_cast<int>(maxWidth-minWidth),
+                 static_cast<int>(maxHeight-minHeight)},
+        minWidth,
+        minHeight};
+}
+
 cv::Mat distortSourceToMatchTarget(
     const cv::Mat &sourceImage, const cv::Mat &targetImage)
 {
@@ -37,49 +91,17 @@ cv::Mat distortSourceToMatchTarget(
                             sourceDescriptors);
 
 
-
-
-    cv::Mat sourceRawKeyPointsImage;
-    cv::drawKeypoints(sourceImage,
-                      sourceKeyPoints,
-                      sourceRawKeyPointsImage);
-    cv::imwrite("sourceRawKeyPoints.png", sourceRawKeyPointsImage);
-    std::clog << "wrote sourceRawKeyPoints.png" << std::endl;
-    cv::Mat targetRawKeyPointsImage;
-    cv::drawKeypoints(targetImage,
-                      targetKeyPoints,
-                      targetRawKeyPointsImage);
-    cv::imwrite("targetRawKeyPoints.png", targetRawKeyPointsImage);
-    std::clog << "wrote targetRawKeyPoints.png" << std::endl;
-
-
     cv::BFMatcher bruteForceMatcher{cv::NORM_HAMMING};
 
     std::vector< std::vector<cv::DMatch> > nearestNeighborMatches;
+    // Last parameter indicates the "k" in knn i.e. get only the 2 best nearest-
+    // neighbor matches. Useful for Lowe's ratio.
     bruteForceMatcher.knnMatch(
             targetDescriptors,
             sourceDescriptors,
             nearestNeighborMatches,
-            2); // Get only the 2 best nearest-neighbor matches. Useful for Lowe's ratio.
+            2);
     std::clog << "finished running matcher" << std::endl;
-
-
-    // Create intermediate vector storing only the best matches.
-    std::vector<cv::DMatch> rawMatches;
-    for (const auto &matchesVec : nearestNeighborMatches)
-    {
-        rawMatches.push_back(matchesVec[0]);
-    }
-    // Display the best nearest-neighbor matches for visual inspection.
-    cv::Mat rawMatchesImage;
-    cv::drawMatches(targetImage,
-                    targetKeyPoints,
-                    sourceImage,
-                    sourceKeyPoints,
-                    rawMatches,
-                    rawMatchesImage);
-    cv::imwrite("rawMatches.png", rawMatchesImage);
-    std::clog << "wrote rawMatches.png" << std::endl;
 
 
     // Get all nearest neighbor matches where the best and second-best matches
@@ -87,27 +109,15 @@ cv::Mat distortSourceToMatchTarget(
     std::vector<cv::DMatch> matches;
     for (const auto &matchVec : nearestNeighborMatches)
     {
-        const auto &best = matchVec[0];
         constexpr const double nearestNeighborRatio = 0.8;
 
+        // Use Lowe's ratio here to filter matches.
         if (matchVec[0].distance < nearestNeighborRatio * matchVec[1].distance)
         {
-            matches.push_back(best);
+            matches.push_back(matchVec[0]);
         }
     }
     std::clog << "filtered matches" << std::endl;
-
-
-    // Display the filtered best nearest-neighbor matches for visual inspection.
-    cv::Mat filteredMatchesImage;
-    cv::drawMatches(targetImage,
-                    targetKeyPoints,
-                    sourceImage,
-                    sourceKeyPoints,
-                    matches,
-                    filteredMatchesImage);
-    cv::imwrite("filteredMatches.png", filteredMatchesImage);
-    std::clog << "wrote filteredMatches.png" << std::endl;
 
 
     // Get the pixel coordinates of each of the matches.
@@ -124,18 +134,55 @@ cv::Mat distortSourceToMatchTarget(
               << " = " << matches.size()/double(sourceKeyPoints.size())
               << std::endl;
 
-    cv::Mat homography = cv::findHomography(targetRansacInliers,
-                                            sourceRansacInliers,
+    cv::Mat homography = cv::findHomography(sourceRansacInliers,
+                                            targetRansacInliers,
                                             cv::RANSAC,
                                             3);
-    std::clog << "found homography matrix" << std::endl;
+    std::clog << "found homography matrix:" << std::endl;
+    std::clog << homography << std::endl;
 
+
+    // Find the bounds and translation vector necessary to align the sourceImage
+    // into the targetImage frame.
+    WarpBounds warpBounds = getWarpedBounds(sourceImage.size(), homography);
+    std::clog << "warped image size " << warpBounds.size << std::endl;
+    std::clog << "warped image min. bounds: "
+              << warpBounds.minWidth << " x " << warpBounds.minHeight << std::endl;
+
+    // Translate the warped source by an amount to get its edges
+    // to line up with the bounds of the image frame, which must be 0.
+    cv::Mat targetFrameHomography = cv::Mat::eye(3, 3, CV_64F);
+    targetFrameHomography.at<double>(0, 2) -= warpBounds.minWidth;
+    targetFrameHomography.at<double>(1, 2) -= warpBounds.minHeight;
+
+    std::clog << "translation matrix to keep source and target images in bound:" << std::endl;
+    std::clog << targetFrameHomography << std::endl;
+
+    // Apply translation transform (captured by targetFrameHomography) AFTER
+    // applying projective transformation i.e. order of operations here matter.
+    homography = cv::Mat(targetFrameHomography*homography);
+    std::clog << homography << std::endl;
+
+    const auto newSize = cv::Size(
+        warpBounds.size.width + warpBounds.minWidth > targetImage.size().width ?
+            warpBounds.size.width
+                :
+            targetImage.size().width-warpBounds.minWidth,
+
+        warpBounds.size.height + warpBounds.minHeight > targetImage.size().height ?
+            warpBounds.size.height
+                :
+            targetImage.size().height-warpBounds.minHeight);
 
     cv::Mat sourceWarped;
-    cv::warpPerspective(sourceImage, sourceWarped, homography, sourceImage.size());
-    cv::imwrite("warpedSource.png", sourceWarped);
+    cv::warpPerspective(sourceImage, sourceWarped, homography, newSize);
+    cv::Mat targetWarped;
+    cv::warpPerspective(targetImage, targetWarped, targetFrameHomography, newSize);
 
-    std::clog << "wrote warpedSource.png" << std::endl;
+    cv::Mat dst;
+    cv::addWeighted(sourceWarped, 0.5, targetWarped, 0.5, 0.0, dst);
+    cv::imwrite("blended.png", dst);
+    std::clog << "wrote blended.png" << std::endl;
 
     return sourceWarped;
 }
