796187e3cf803e419404dbff81377179e881b472
Pratool Gadtaula <5598110+Pratool@users.noreply.github.com>
2019-10-27 11:32:55 -0400

    feat(Solvers): Numeric type enforcement on findHomographyWithRansac
    * Make Solvers header-only

--- cpp/Solvers/include/Solvers/Ransac.hpp
+++ cpp/Solvers/include/Solvers/Ransac.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <numeric>
+#include <random>
 #include <type_traits>
 #include <utility>
 #include <vector>
@@ -13,12 +14,6 @@ cv::Mat distortSourceToMatchTarget(
 
 namespace pcv
 {
-
-Eigen::Matrix3d
-findHomographyWithRansac(
-    std::vector<std::pair<cv::Point2f, cv::Point2f>> correspondences,
-    double reprojectionErrorThreshold,
-    std::size_t iterations);
 
 template<class NumericType>
 double getReprojectionError(
@@ -136,6 +131,88 @@ findHomographyWithDirectLinearTransform(
     Eigen::JacobiSVD<decltype(joinedPoints)> svd(joinedPoints, Eigen::ComputeFullV);
 
     return Eigen::Matrix<double, 3, 3, Eigen::RowMajor>(svd.matrixV().col(8).data());
+}
+
+template<class NumericType>
+Eigen::Matrix3d
+findHomographyWithRansac(
+    std::vector<std::pair<
+        cv::Point_<NumericType>, cv::Point_<NumericType> >>
+        correspondences,
+    double reprojectionErrorThreshold,
+    std::size_t iterations)
+{
+    static_assert(std::is_floating_point<NumericType>::value,
+                  "Not floating point.");
+
+    using DataType = std::pair<
+        cv::Point_<NumericType>, cv::Point_<NumericType> >;
+
+    if (correspondences.size() < 4)
+    {
+        throw std::runtime_error("There must be at least 4 point correspondences"
+                                 "to calculate a homography.");
+    }
+
+    // There will at minimum be 4 inliers as 4 randomly selected points are
+    // inliers.
+    std::size_t bestInlierCount = 0;
+
+    // The default value of bestCost shall never be used. It should always
+    // get overwritten otherwise discarded.
+    double bestCost = 0;
+
+    Eigen::Matrix3d bestModel{};
+
+    // Always run at least one iteration of RANSAC.
+    for (std::size_t iteration = 0; iteration < iterations+1; ++iteration)
+    {
+        std::vector<DataType> tmpInliers;
+        for (std::size_t minDataIter = 0; minDataIter < 4; ++minDataIter)
+        {
+            std::uniform_int_distribution<std::size_t> indexRandomizer(0, correspondences.size());
+            std::random_device randomDevice;
+            tmpInliers.push_back(correspondences[indexRandomizer(randomDevice)]);
+        }
+
+        auto tmpModel = pcv::findHomographyWithDirectLinearTransform(tmpInliers);
+        std::size_t tmpInlierCount = 0;
+
+        const auto tmpTotalCost = std::accumulate(
+            std::cbegin(correspondences),
+            std::cend(correspondences),
+            0.0,
+            [tmpModel, reprojectionErrorThreshold, &tmpInlierCount](double accumulator, DataType dataIter)
+            {
+                const auto &reprojectionError = getReprojectionError(tmpModel, dataIter);
+
+                // Do not count this point's reprojection error toward the total
+                // cost because it is an outlier.
+                if (reprojectionError > reprojectionErrorThreshold)
+                {
+                    return accumulator;
+                }
+
+                ++tmpInlierCount;
+                return accumulator + reprojectionError;
+            });
+
+
+        // If the current model has most inliers insofar, or if the the number
+        // of inliers matches the most inliers insofar and the cost has been
+        // reduced, then update the best model with the current model.
+        if (tmpInlierCount > bestInlierCount
+           || ((tmpInlierCount == bestInlierCount) && tmpTotalCost < bestCost))
+        {
+            bestCost = tmpTotalCost;
+            bestInlierCount = tmpInlierCount;
+
+            // Ensure that the bottom-right corner of the 3x3 matrix is 1 and
+            // divide element-wise through the matrix.
+            bestModel = tmpModel / tmpModel(2,2);
+        }
+    }
+    return bestModel;
 }
 
 } // end namespace utility
