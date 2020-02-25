df48d3344b7bee714b2b00b8557f66670e923a77
Pratool Gadtaula <5598110+Pratool@users.noreply.github.com>
2020-02-25 01:28:24 -0500

    fix(python): O(lg(n)) inclusions rewrite for clarity

--- python/intersection.py
+++ python/intersection.py
@@ -21,57 +21,14 @@ def main():
     poly0 = ConvexPolygon([(18, 76), (45, 91), (91, 69), (43, 13), (21, 23)])
     poly1 = ConvexPolygon([(76, 92), (88, 74), (89, 57), (45, 9), (36, 80)])
 
-    lgn_avgs = []
-    min_input_size = 5
-    max_input_size = 50000
+    run_and_plot(quad0, quad1)
+    run_and_plot(poly0, poly1)
+    run_and_plot(gen_random_convex_poly(11),
+                 gen_random_convex_poly(10))
 
-    size_range = range(min_input_size, max_input_size, 500)
-    for input_size in size_range:
-        lgn = []
-        pt = Vertex((50.1, 50.5))
-        #pt = Vertex((101.3, 50.5))
 
-        for iters in range(5):
-            #tmp_poly0 = gen_random_convex_poly(input_size, x_min=25, x_max=75)
-            tmp_poly0 = gen_random_convex_poly(input_size)
-            try:
-                start = dt.utcnow()
-                is_inside(pt, tmp_poly0)
-                end = dt.utcnow()
-                lgn.append(end-start)
-                lgn[-1] = lgn[-1].seconds + lgn[-1].microseconds*1e-6
-            except IndexError:
-                print('error at {} vertices'.format(input_size))
-            """
-            if lgn[-1] > 0.0002:
-                ax = plt.gca()
-                tmp_poly0.plot(ax)
-                ax.plot(pt.x(), pt.y(), 'o')
-                plt.show()
-            """
-
-            # is_inside_n2() is too slow to keep up with trying to sample
-            # is_inside() at larger input sizes.
-            #start = dt.utcnow()
-            #is_inside_n2(pt, tmp_poly0)
-            #end = dt.utcnow()
-            #n2.append(end-start)
-            #n2[-1] = n2[-1].seconds + n2[-1].microseconds*1e-6
-        try:
-            lgn_avgs.append(sum(lgn)/float(len(lgn)))
-        except Exception:
-            lgn_avgs.append(0)
-
-    ax = plt.gca()
-    tmp_poly0.plot(ax)
-    ax.plot(pt.x(), pt.y(), 'o')
-    plt.show()
-
-    xs = [x for x in size_range]
-    plt.plot(xs, [5.5e-5 / 16.0 * math.log2(x) for x in xs])
-    plt.plot(xs, lgn_avgs)
-    plt.show()
-
+def find_mismatches():
+    quad0 = ConvexPolygon([(5, 14), (9, 8), (5, 3), (1, 7)])
     mismatches = []
     # Compare straight-forward O(n) implementation with O(lg(n)) implementation.
     grid_size = 10
@@ -83,10 +40,13 @@ def main():
             if res != res2:
                 mismatches.append((pt, res, res2))
     print(len(mismatches))
-
-    #run_and_plot(quad0, quad1)
-    #run_and_plot(poly0, poly1)
-
+    for m in mismatches[:10]:
+        print(m)
+        res = is_inside(m[0], quad0, log=True)
+        axes = plt.gca()
+        quad0.plot(axes)
+        plt.plot(m[0].x(), m[0].y(), 'o')
+        plt.show()
 
 
 def run_and_plot(poly0, poly1):
@@ -96,19 +56,21 @@ def run_and_plot(poly0, poly1):
     intersect_polys(poly0, poly1, is_inside).plot(ax)
     plt.show()
 
+    ax = plt.gca()
+    poly0.plot(ax)
+    poly1.plot(ax)
+    intersect_polys(poly0, poly1).plot(ax)
+    plt.show()
+
 
 def gen_random_convex_poly(number_of_vertices, radius=50.0, x_min=0, x_max=100, y_min=0, y_max=100):
+    """Not truly random. Mostly an observation that yields convex polygons.
+    """
     offsets = sorted([random() * 2.0 * math.pi for i in range(number_of_vertices)])
     x_offset = x_min + (x_max-x_min)*random()
     y_offset = y_min + (y_max-y_min)*random()
 
     return ConvexPolygon([(radius*math.cos(theta) + x_offset, radius*math.sin(theta) + y_offset) for theta in offsets])
-
-
-def gen_random_poly(number_of_vertices):
-    # Note that this result is not necessarily convex.
-    return Polygon([(int(random() * 100), int(random() * 100))
-                    for i in range(number_of_vertices)])
 
 
 class Vertex:
@@ -152,7 +114,7 @@ class Vertex:
         return '<Vertex {}>'.format(str(self))
 
     def __str__(self):
-        return str(self.coordinates)
+        return '({:.2f}, {:.2f})'.format(*self.coordinates)
 
 
 class PolygonIter:
@@ -184,10 +146,6 @@ class PolygonIter:
         return_index = self.index + skips
         if (return_index >= len(self.polygon.vertices) or
             return_index <= -1):
-            print(self.index, skips, return_index)
-            #ax = plt.gca()
-            #self.polygon.plot(ax)
-            #plt.show()
             raise IndexError('Cannot advance beyond bounds of corresponding '
                              'polygon vertices container.')
         return return_index
@@ -214,10 +172,7 @@ class PolygonIter:
         return str(self.polygon.get_vertex_at(self.index))
 
 
-class ConvexPolygon:
-    """This class encapsulates properties convex polygons posess in the
-    most efficient runtime complexity possible. Class methods should perform
-    actions in-place."""
+class Polygon:
 
     def __init__(self, vertices):
         """Vertices are connected by line segments in clockwise order to
@@ -231,8 +186,6 @@ class ConvexPolygon:
         for i in range(len(self.vertices)):
             self.vertices[i - 1].set_next(self.vertices[i])
             self.vertices[i].set_prev(self.vertices[i - 1])
-
-        self.c = 0
 
     def __repr__(self):
         return '->'.join([str(v) for v in self.vertices])
@@ -269,6 +222,11 @@ class ConvexPolygon:
         ys = [v.y() for v in self.vertices] + [self.vertices[0].y()]
         mplib_axes.plot(xs, ys, 'o-')
         return mplib_axes
+
+class ConvexPolygon(Polygon):
+    """This class encapsulates properties convex polygons posess in the
+    most efficient runtime complexity possible. Class methods should perform
+    actions in-place."""
 
     def _search_vertex(self, comparator, start=0, end=None):
         """Retrieve vertex to exploit convexity within the polygon's vertex
@@ -339,26 +297,6 @@ class ConvexPolygon:
         """
         return self._search_vertex(lambda v0, v1: v0.y() < v1.y())
 
-    def get_right_side(self):
-        bottom = self.get_bottom_vertex()
-        tmp = self.get_top_vertex()
-        side = []
-        while tmp != bottom:
-            side.append(tmp)
-            tmp = tmp.next
-        side.append(tmp)
-        return side
-
-    def get_left_side(self):
-        top = self.get_top_vertex()
-        tmp = self.get_bottom_vertex()
-        side = []
-        while tmp != top:
-            side.append(tmp)
-            tmp = tmp.next
-        side.append(tmp)
-        return side
-
 # BEGINNING EXTREMELY TERSE FUNCTIONS
 def line_intersect(p0, p1, p2, p3, err=0):
     lhs = np.array([[p0.y() - p1.y(), p1.x() - p0.x()], [p2.y() - p3.y(), p3.x() - p2.x()]])
@@ -398,15 +336,19 @@ and_op = lambda x, y: x and y
 pt_vertices = lambda pt, vertices: reduce(
     or_op, [pt_eq(pt, Vertex(v))
             for v in vertices]) if len(vertices) > 0 else False
-is_left = lambda pt, v0, v1: ((v0.x() - pt.x()) * (v1.y() - pt.y()) - (v0.y() - pt.y()) *
-                              (v1.x() - pt.x())) <= 0
+# pt is the point to test, v0, v1 are endpoints of the line segment.
+signed_area = lambda pt, v0, v1: ((v0.x() - pt.x()) * (v1.y() - pt.y()) - (v0.y() - pt.y()) *
+                              (v1.x() - pt.x()))
+is_left = lambda pt, v0, v1: signed_area(pt, v0, v1) <= 0
 is_inside_n2 = lambda pt, poly: reduce(and_op, [
     is_left(pt, poly.vertices[i - 1], poly.vertices[i])
     for i in range(len(poly.vertices))
 ])
 
+# END EXTREMELY TERSE FUNCTIONS
 
-def is_inside(point, polygon):
+
+def is_inside(point, polygon, log=False):
     """Determines if a point lies inside a convex polygon.
 
     Args:
@@ -418,52 +360,89 @@ def is_inside(point, polygon):
     Returns:
         Boolean value where True indicates the point is inside the polygon.
     """
-    # perform input conditioning here.
     top = polygon.get_top_vertex()
     bottom = polygon.get_bottom_vertex()
 
+    # If the point's y-value is not within the y-value bounds of the polygon
     if point.y() > top.get_value().y() or point.y() < bottom.get_value().y():
         return False
 
-    # Vertex at tail of left polyline's line segment where vertical bounds
-    # encompass point.
-    left = _is_inside(point, polygon, bottom, top, lambda u, v: u.y() >= v.y()).get_value()
+    # Condition input before next stage.
+    offset = -bottom.index
+    start_bottom = bottom.index + offset
+    if top.index + offset < 0:
+        end_top = len(polygon.vertices) - (
+                  -(top.index + offset) % len(polygon.vertices))
+    else:
+        end_top = top.index + offset
+    comparator = lambda v0, v1: v1.y() > v0.y()
+    offset = -offset
 
-    # Vertex at tail of right polyline's line segment where vertical bounds
-    # encompass point.
-    right = _is_inside(point, polygon, top, bottom, lambda u, v: u.y() <= v.y()).get_value()
+    # Find polygon line segment that intersects a horizontal line that passes
+    # through the point from the left side of the polygon.
+    lhs_tail = get_segment_tail(point, polygon, comparator,
+                                start_bottom, end_top, offset)
 
-    return (is_left(point, left, left.next_vertex()) and
-            is_left(point, right, right.next_vertex()))
+    # Condition input before next stage.
+    offset = -top.index
+    start_top = top.index + offset
+    if bottom.index + offset < 0:
+        end_bottom = len(polygon.vertices) - (
+                  -(bottom.index + offset) % len(polygon.vertices))
+    else:
+        end_bottom = bottom.index + offset
+    comparator = lambda v0, v1: v1.y() < v0.y()
+    offset = -offset
 
+    # Find polygon line segment that intersects a horizontal line that passes
+    # through the point from the right side of the polygon.
+    rhs_tail = get_segment_tail(point, polygon, comparator, start_top,
+                                end_bottom, offset)
 
-def _is_inside(point, polygon, start_iter, end_iter, comparator):
-        if start_iter == end_iter:
-            return start_iter
-        if start_iter - end_iter == 1:
-            if comparator(start_iter.get_value(), end.get_value()):
-                return start_iter
-            return end_iter
-
-        mid = start_iter + polygon.get_iterator_wrapped_in_bounds( int((end_iter - start_iter) / 2.0) )
-        mid_iter = polygon.get_iterator(mid)
-        after_mid_iter = polygon.get_iterator_wrapped_in_bounds(mid + 1)
-        before_mid_iter = polygon.get_iterator_wrapped_in_bounds(mid - 1)
-
-        if comparator(after_mid_iter.get_value(), point) and not comparator(mid_iter.get_value(), point):
-            return mid_iter
-        elif comparator(mid_iter.get_value(), point):
-            return _is_inside(point, polygon, start_iter, before_mid_iter, comparator)
-        else:
-            return _is_inside(point, polygon, after_mid_iter, end_iter, comparator)
+    return (signed_area(point, lhs_tail.next_vertex(), lhs_tail) >= 0 and
+            is_left(point, rhs_tail, rhs_tail.next_vertex()))
 
 
+def get_segment_tail(point, polygon, comparator, start, end, offset):
+    """Assumes that start indicates an index value of 0 at the beginning of
+    recursion. At no point shall the value of end be less than the value of
+    start. Indices refer to the index within the polygon vertex list. Search
+    cases may not neatly have the start index as 0, in which case an offset is
+    added to bring both start and end indices to the intended indices in the
+    polygon vertex list.
+    """
+    # Permit access to the vertex data.
+    start_iter = polygon.get_iterator_wrapped_in_bounds(start+offset)
+    end_iter = polygon.get_iterator_wrapped_in_bounds(end+offset)
+
+    if start == end:
+        return start_iter.get_value()
+
+    if end-start == 1:
+        if comparator(start_iter.get_value(), end_iter.get_value()):
+            return start_iter.get_value()
+        return end_iter.get_value()
+
+    mid = start + int((end - start) / 2.0)
+    mid_point = polygon.get_iterator_wrapped_in_bounds(mid + offset).get_value()
+    prev_point = mid_point.prev_vertex()
+    next_point = mid_point.next_vertex()
+
+    if comparator(prev_point, point) and comparator(point, mid_point):
+        return prev_point
+    if comparator(mid_point, point):
+        return get_segment_tail(point, polygon, comparator, mid, end, offset)
+    return get_segment_tail(point, polygon, comparator, start, mid - 1, offset)
+
+
+# This function has many bugs. Use with caution: does not work as advertised!
 def intersect_polys(poly0, poly1, is_inside=is_inside_n2):
     inter = []
 
     poly0v = poly0.vertices[0]
     poly1v = poly1.vertices[0]
 
+    # Prevent faulty logic from causing an infinite loop.
     max_iters = 2 * (len(poly0.vertices) + len(poly1.vertices))
     c = 0
 
@@ -471,7 +450,7 @@ def intersect_polys(poly0, poly1, is_inside=is_inside_n2):
         if is_inside(poly0v, poly1):
             if pt_vertices(poly0v, inter):
                 break
-            inter.append(poly0v.coord)
+            inter.append((poly0v.x(), poly0v.y()))
 
         tmp_inters = []
         for p1v in poly1.vertices:
@@ -481,8 +460,8 @@ def intersect_polys(poly0, poly1, is_inside=is_inside_n2):
 
         # sort by euclidean distance from poly0v
         tmp_inters = sorted(tmp_inters,
-                            key=lambda x: (poly0v.x - x[0])**2 +
-                            (poly0v.y - x[1])**2)
+                            key=lambda x: (poly0v.x() - x[0])**2 +
+                            (poly0v.y() - x[1])**2)
 
         broke = False
         for i in tmp_inters:
