5c3b1db1504d492fe39f2206e53a84761418f0dd
Pratool Gadtaula <5598110+Pratool@users.noreply.github.com>
2023-02-19 12:37:30 -0500

    wip

--- cpp/Samples/source/sobel.cpp
+++ cpp/Samples/source/sobel.cpp
@@ -14,21 +14,23 @@ int main()
 
   rgb8_image_t convolved(img);
 
-  float gaussian[] = { 0.00022923296f, 0.0059770769f, 0.060597949f, 0.24173197f, 0.38292751f,
-                        0.24173197f, 0.060597949f, 0.0059770769f, 0.00022923296f};
+  std::array<float, 9> gaussian = {
+    0.075, 0.075, 0.075,
+    0.075, 0.4, 0.075,
+    0.075, 0.075, 0.075 };
 
-  kernel_1d_fixed<float, 9> guassian_kernel(gaussian, 4);
+  kernel_2d_fixed<float, 9> guassian_kernel(gaussian.begin(), 1, 1);
+  convolve_rows_fixed<rgb32f_pixel_t>(const_view(convolved), guassian_kernel, view(convolved));
 
+  /*
   float sobel_x[] = { 1.0f, 0.0f, -1.0f, 2.0f, 0.0f, -2.0f, 1.0f, 0.0f, -1.0f };
   kernel_1d_fixed<float, 9> sobel_x_kernel(sobel_x, 4);
-
   float sobel_y[] = { 1.0f, 2.0f, 1.0f, 0.0f, 0.0f, 0.0f, -1.0f, -2.0f, -1.0f };
   kernel_1d_fixed<float, 9> sobel_y_kernel(sobel_y, 4);
-
-  convolve_rows_fixed<rgb32f_pixel_t>(const_view(convolved), guassian_kernel, view(convolved));
   convolve_cols_fixed<rgb32f_pixel_t>(const_view(convolved), guassian_kernel, view(convolved));
   convolve_rows_fixed<rgb32f_pixel_t>(const_view(convolved), sobel_x_kernel, view(convolved));
   convolve_cols_fixed<rgb32f_pixel_t>(const_view(convolved), sobel_y_kernel, view(convolved));
+  */
 
   write_view("test-blur.png", view(convolved), boost::gil::png_tag{});
 
